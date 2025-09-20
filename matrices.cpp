2d0169161ef4e48d8d486167de7f384a9519a1ec
fernandocre <fernando_hu@outlook.com>
2025-09-19 20:54:51 -0500

    Update matrices.cpp

--- matrices.cpp
+++ matrices.cpp
@@ -1,51 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void multi(int (*m)[3][3]) {
-    int* A = (int*)m[0]; 
-    int* B = (int*)m[1]; 
-    int* C = (int*)m[2]; 
+void multi(int (*p)[3][3]) {
+    int *P = &p[0][0][0]; // A[0]
+    int *Q = &p[1][0][0]; // A[1]
+    int *R = &p[2][0][0]; // A[2] (resultado)
 
-    // filas de A
-    for (int fila = 0; fila < 3; fila++) {
-        int* filaA = A + fila * 3;   // puntero al inicio de la fila en A
+    for (int i = 0; i < 3; ++i) {                // filas de A
+        int *filaA = P + i*3;
+        int *R_ini = R + i*3;                    // R-ini (inicio de esa fila en C)
 
-        // columnas de B
-        for (int col = 0; col < 3; col++) {
+        for (int j = 0; j < 3; ++j) {            // columnas de B
             int suma = 0;
-            int* elemA = filaA;     
-            int* elemB = B + col;   
-
-            // filaA x columnaB
-            for (int k = 0; k < 3; k++) {
-                suma += (*elemA) * (*elemB);
-                elemA++;        
-                elemB += 3;     
+            int *a = filaA;                      // avanza por la fila de A
+            int *b = Q + j;                      // arranca en B[0][j]
+            for (int k = 0; k < 3; ++k) {
+                suma += (*a) * (*b);
+                ++a;                             // siguiente de la fila
+                b += 3;                          // siguiente de la columna (salto de 3)
             }
-
-            *C = suma; // guardo en C
-            C++;       // avanzo al siguiente elemento de C
+            R_ini[j] = suma;                     // escribe en C[i][j]
         }
     }
 }
 
-
 int main() {
     int a[3][3][3] = {
-        { {1,2,3}, {4,5,6}, {7,8,9} },   
-        { {9,8,7}, {6,5,4}, {3,2,1} },   
-        { {0,0,0}, {0,0,0}, {0,0,0} }    
+        { {1,2,3}, {4,5,6}, {7,8,9} },     // A[0]
+        { {9,8,7}, {6,5,4}, {3,2,1} },     // A[1]
+        { {0,0,0}, {0,0,0}, {0,0,0} }      // A[2] (resultado)
     };
 
     multi(a);
 
-    cout << "matriz resultado C:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << a[2][i][j] << " "; 
-        }
-        cout << endl;
+    cout << "matriz resultado C:\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) cout << a[2][i][j] << ' ';
+        cout << '\n';
     }
-
-    return 0;
 }
