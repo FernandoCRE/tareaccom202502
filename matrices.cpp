98085ded99e53a269655106c4ea92a53b9cc88d2
fernandocre <fernando_hu@outlook.com>
2025-09-20 08:20:43 -0500

    Update matrices.cpp

--- matrices.cpp
+++ matrices.cpp
@@ -6,29 +6,29 @@ void multi(int (*p)[3][3]) {
     int *Q = **(p+1);
     int *R = **(p+2);
 
-    for (int i = 0; i < 3; ++i) {                // 
+    for (int i = 0; i < 3; ++i) {                
         int *filaA = P + i*3;
-        int *R_ini = R + i*3;                    // R-ini (inicio de esa fila en C)
+        int *R_ini = R + i*3;                    // R-ini inicio de esa fila en C
 
         for (int j = 0; j < 3; ++j) {            // columnas de B
             int suma = 0;
             int *a = filaA;                      // avanza por la fila de A
-            int *b = Q + j;                      // arranca en B[0][j]
+            int *b = Q + j;                      
             for (int k = 0; k < 3; ++k) {
                 suma += (*a) * (*b);
                 ++a;                             // siguiente de la fila
-                b += 3;                          // siguiente de la columna (salto de 3)
+                b += 3;                          // siguiente de la columna salto de 3
             }
-            R_ini[j] = suma;                     
+            *(R_ini+j) = suma;                     
         }
     }
 }
 
 int main() {
     int a[3][3][3] = {
-        { {1,2,3}, {4,5,6}, {7,8,9} },     // A[0]
-        { {9,8,7}, {6,5,4}, {3,2,1} },     // A[1]
-        { {0,0,0}, {0,0,0}, {0,0,0} }      // A[2] 
+        { {1,2,3}, {4,5,6}, {7,8,9} },     
+        { {9,8,7}, {6,5,4}, {3,2,1} },     
+        { {0,0,0}, {0,0,0}, {0,0,0} }      
     };
 
     multi(a);
